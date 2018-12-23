a7fea316a5c421555a73e044bc86508f3371788d
mbIM <lina.alina.fedotova@gmail.com>
2018-12-23 22:37:15 +0300

    ...

--- BotGetCourse.py
+++ BotGetCourse.py
@@ -1,10 +1,8 @@
 import vk_api
 import random
-import requests
-from bs4 import BeautifulSoup
-from datetime import datetime
 import sys
 import lxml
+from грп import *
 
 token = "3d02e045e965c29c4c4a04606979e5c5b0aa3372fcd07a97d4fcfec0c799f52e46355eacac5affb5d9eb9"
 
@@ -12,26 +10,26 @@ vk = vk_api.VkApi(token=token)
 
 vk._auth_token()
 
+flags = {
+    "-д" : "R01235",
+    "-ю" : "R01375",
+    "-ф" : "R01035"
+}
+
+names = {
+    '-д' : ' рублей за 1 доллар',
+    '-ю' : ' рублей за 1 юань',
+    '-ф' : ' рублей за один фунт'
+}
+
 while True:
     messages = vk.method("messages.getConversations", {"count": 21, "filter": "unanswered"})
     if messages["count"] > 0:
         id = messages["items"][0]['last_message']['from_id']
         #vk.method("messages.send",{"user_id": id, "random_id": random.randint(1, 200000), "message": "Привет!"})
-        msg = messages["items"][0]["last_message"]["text"]
-        if msg == "курс":
+        msg = messages["items"][0]["last_message"]["text"].lower()
+        flag = flags.get(msg[5: ])
+        name = names.get(msg[5: ])
 
-            url = "http://www.cbr.ru/scripts/XML_daily.asp?"
-
-            today = datetime.today()
-            today = today.strftime("%d/%m/%Y")
-
-            payload = {"date_req" : today}
-
-            responce = requests.get(url, params=payload)
-
-            xml = BeautifulSoup(responce.content, "lxml")
-
-            def getCourse (id):
-                return xml.find("valute", {'id': id}).value.text
-
-            vk.method("messages.send",{"user_id": id, "random_id": random.randint(1, 200000), "message": (getCourse("R01235") + " рублей за 1 доллар")})
+        if len(msg) == 7 and msg[0:4] == "курс" and flag != None and name != None:
+         vk.method("messages.send",{"user_id": id, "random_id": random.randint(1, 200000), "message": getCourse(flag) + name})
